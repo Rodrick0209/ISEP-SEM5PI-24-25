97d078b62ed5224727346b16f6fbd3cd2c7ff36b
Mario Joao <1221019@isep.ipp.pt>
2024-10-19 19:50:55 +0100

    License number domain validation #13

--- MastersData/src/Domain/StaffMembers/LicenseNumber.cs
+++ MastersData/src/Domain/StaffMembers/LicenseNumber.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Text.RegularExpressions;
 using DDDSample1.Domain.Shared;
 
 
@@ -19,9 +20,17 @@ namespace DDDSample1.Domain.StaffMembers
 
         private void validateLicenseNumber(string licenseNumber)
         {
+            // Permite que licenseNumber seja vazio ou atenda aos formatos:
+            // 1. 5 dígitos (xxxxx)
+            // 2. E-5 dígitos (E-xxxxx)
+            string pattern = @"^(E-\d{5}|\d{5})?$";
 
-
+            if (!Regex.IsMatch(licenseNumber, pattern))
+            {
+                throw new ArgumentException("License number must be in the format 'xxxxx' or 'E-xxxxx', or it can be empty.");
+            }
         }
+
 
 
 
