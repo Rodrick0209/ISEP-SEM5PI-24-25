e9a9abc929a6f557b325191d35abbbf61afe904a
Mario Joao <1221019@isep.ipp.pt>
2024-10-19 19:40:10 +0100

    Conflict Resolved

---
+++ MastersData/src/Domain/StaffMembers/Staff.cs
@@ -0,0 +1,75 @@
+#nullable enable
+using System;
+using System.Collections.Generic;
+using DDDSample1.Domain.Shared;
+using DDDSample1.Domain.Utils;
+using DDDSample1.Domain.Availability;
+using DDDSample1.Domain.Specializations;
+using Microsoft.Extensions.Logging.Configuration;
+using Domain.StaffMembers;
+
+namespace DDDSample1.Domain.StaffMembers
+{
+    public class Staff : Entity<StaffId>, IAggregateRoot
+    {
+        public FullName FullName { get; private set; }
+        public LicenseNumber LicenseNumber { get; private set; }
+        public SpecializationId SpecializationId { get; private set; }
+        public AvailabilitySlotsId AvailabilitySlotsId { get; private set; }
+        public Email Email { get; private set; }
+        public PhoneNumber PhoneNumber { get; private set; }
+        public Category Category { get; private set; }
+        public User.User? User { get; private set; }
+
+        private Staff()
+        {
+        }
+
+        public Staff(StaffId staffId, string fullName, string licenseNumber, SpecializationId specializationId, AvailabilitySlotsId availabilitySlotsId, string email, string phoneNumber, string category)
+        {
+            this.Id = staffId;
+            this.FullName = new FullName(fullName);
+            this.LicenseNumber = new LicenseNumber(licenseNumber);
+            this.SpecializationId = specializationId;
+            this.AvailabilitySlotsId = availabilitySlotsId;
+            this.Email = new Email(email);
+            this.PhoneNumber = new PhoneNumber(phoneNumber);
+            this.Category = Enum.Parse<Category>(category);
+        }
+
+        public void ChangeFullName(FullName fullName)
+        {
+            this.FullName = fullName;
+        }
+
+        public void ChangeLicenseNumber(LicenseNumber licenseNumber)
+        {
+            this.LicenseNumber = licenseNumber;
+        }
+
+        public void ChangeSpecialization(SpecializationId specializationId)
+        {
+            this.SpecializationId = specializationId;
+        }
+
+        public void ChangeAvailabilitySlots(AvailabilitySlotsId availabilitySlots)
+        {
+            this.AvailabilitySlotsId = availabilitySlots;
+        }
+
+        public void ChangeEmail(Email email)
+        {
+            this.Email = email;
+        }
+
+        public void ChangePhoneNumber(PhoneNumber phoneNumber)
+        {
+            this.PhoneNumber = phoneNumber;
+        }
+        public void ChangeCategory(Category category)
+        {
+            this.Category = category;
+        }
+    
+    }
+}
---
+++ MastersData/src/Domain/StaffMembers/StaffIdGeneratorService.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+using DDDSample1.Domain.StaffMembers;
+using Domain.StaffMembers;
+
+namespace DDDSample1.Domain.StaffMembers
+{
+    public class StaffIdGeneratorService
+    {
+        private readonly Dictionary<Category, int> _sequentialNumbers;
+
+        public StaffIdGeneratorService()
+        {
+            // Inicializa o dicionário com contadores separados por categoria
+            _sequentialNumbers = new Dictionary<Category, int>
+            {
+                { Category.Doctor, 0 },           // Contador para médicos e internos
+                { Category.InternDoctor, 0 },      // Usará o mesmo contador que o médico
+                { Category.Nurse, 0 },
+                { Category.Technician, 0 }
+            };
+        }
+
+        public StaffId GenerateStaffId(Category category, DateTime recruitmentDate)
+        {
+            // Determina o prefixo com base na categoria
+            string prefix = category switch
+            {
+                Category.Doctor => "D",
+                Category.InternDoctor => "D", // InternDoctor também usa "D"
+                Category.Nurse => "N",
+                _ => "O" // Outros (Technician ou qualquer outra categoria)
+            };
+
+            // Extrai o ano de recrutamento
+            string year = recruitmentDate.Year.ToString();
+
+            // Incrementa o número sequencial para a categoria correspondente
+            int sequentialNumber = _sequentialNumbers[category]++;
+            string sequentialNumberString = sequentialNumber.ToString("D5"); // Formato de 5 dígitos
+
+            /// Concatena prefixo, ano e número sequencial
+            string staffIdValue = $"{prefix}{year}{sequentialNumberString}";
+
+            // Retorna um novo objeto StaffId
+            return new StaffId(staffIdValue);
+        }
+    }
+}
