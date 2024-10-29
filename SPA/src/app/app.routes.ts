3c85630090818c92a582039e2139c11d667ef5fe
Rodrigo <1221083@isep.ipp.pt>
2024-10-29 21:18:01 +0000

    Added about and Contact components #25

--- SPA/src/app/app.routes.ts
+++ SPA/src/app/app.routes.ts
@@ -1,6 +1,13 @@
 import { Routes } from '@angular/router';
 import { CubeComponent } from './cube/cube.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
 
 export const routes: Routes = [
+    { path : '', redirectTo: 'home', pathMatch: 'full' },
+    { path: 'home', component: HomeComponent },
+    { path: 'about', component: AboutComponent },
+    { path: 'contact', component: ContactComponent },
     { path: 'cube', component: CubeComponent },
 ];
