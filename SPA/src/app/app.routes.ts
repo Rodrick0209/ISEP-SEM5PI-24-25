6d313fbc8b462b14196b7fefd91d4275a1e9d115
Rodrigo <1221083@isep.ipp.pt>
2024-10-31 11:50:25 +0000

    Fixed Components to a folder #25

--- SPA/src/app/app.routes.ts
+++ SPA/src/app/app.routes.ts
@@ -1,12 +1,12 @@
 import { Routes } from '@angular/router';
-import { CubeComponent } from './cube/cube.component';
-import { LoginComponent } from './login/login.component';
-import { HomeComponent } from './home/home.component';
-import { AboutComponent } from './about/about.component';
-import { ContactComponent } from './contact/contact.component';
-import { RegisterComponent } from './register/register.component';
-import { ConfirmationErrorComponent } from './confirmation-error/confirmation-error.component';
-import { ConfirmationSuccessComponent } from './confirmation-success/confirmation-success.component';
+import { CubeComponent } from './components/cube/cube.component';
+import { LoginComponent } from './components/login/login.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { RegisterComponent } from './components/register/register.component';
+import { ConfirmationErrorComponent } from './components/confirmation-error/confirmation-error.component';
+import { ConfirmationSuccessComponent } from './components/confirmation-success/confirmation-success.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'home', pathMatch: 'full' },
