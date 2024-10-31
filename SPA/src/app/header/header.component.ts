a726428e2448a48462dfce0a81bf22cf9cc66ea1
Miguel Oliveira <1211281@isep.ipp.pt>
2024-10-31 11:58:08 +0000

    login changes #37

--- SPA/src/app/header/header.component.ts
+++ SPA/src/app/header/header.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AuthService } from '../services/auth.service'; // Adjust path as needed
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+  isLoggedIn: boolean = false;
 
+  constructor(private authService: AuthService) {}
+
+  ngOnInit(): void {
+    this.isLoggedIn = this.authService.isLoggedIn();
+  }
+
+  logout(): void {
+    this.authService.clearToken();
+    this.isLoggedIn = false;
+  }
 }
