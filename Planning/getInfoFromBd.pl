bee4c8ad88aba863bef1709d64ab5750d007f77b
Rodrigo <1221083@isep.ipp.pt>
2024-11-18 18:42:11 +0000

    Added get all surgeries to catch from back-end #33

--- Planning/getInfoFromBd.pl
+++ Planning/getInfoFromBd.pl
@@ -9,6 +9,13 @@
 :- use_module(library(http/json)).
 
 :- dynamic agenda_staff/3.  % Define o predicado dinâmico
+:- dynamic surgery/4.        % Declare the surgery/4 predicate as dynamic
+:- dynamic surgery_id/2.     % Declare the surgery_id/2 predicate as dynamic
+:- dynamic surgery_counter/1.% Declare the global counter for surgery IDs
+
+% Initialize the counter if it doesn't exist
+:- (retract(surgery_counter(_)) -> true ; assertz(surgery_counter(1))).
+
 
 %% Função principal para salvar a agenda dos medicos num ficheiro
 saveDoctorSchedules(Data) :-
@@ -28,6 +35,33 @@ saveDoctorSchedules(Data) :-
         )
     ).
 
+% Generate the next surgery ID
+next_surgery_id(SurgeryId) :-
+    retract(surgery_counter(N)),                
+    format(atom(SurgeryId), 's10000~w', [N]),        
+    NewN is N + 1,                              % Increment the counter
+    assertz(surgery_counter(NewN)).             % Update the counter
+
+% Salva os doutores
+saveSurgeries(Data) :-
+    % Percorre a lista de dados e adiciona como factos à base de dados Prolog
+    forall(member(Surgery, Data),
+        (
+            forall(member(Surgery, Data),
+                (
+                    next_surgery_id(SurgeryId),
+                    Surgery = _{fullName: SurgeryName, preparationPhase: PreparationPhase, surgeryPhase: SurgeryPhase, cleaningPhase: CleaningPhase},
+                    PreparationPhase = _{duration: PreparationDuration},
+                    SurgeryPhase = _{duration: SurgeryDuration},
+                    CleaningPhase = _{duration: CleaningDuration},
+                    atom_string(SurgeryName, SurgeryNameStr),  % Converte SurgeryName de string para átomo
+                    atom_string(SurgeryId, SurgeryIdStr),  % Converte SurgeryId de string para átomo
+                    assertz(surgery(SurgeryNameStr, PreparationDuration, SurgeryDuration, CleaningDuration)),
+                    assertz(surgery_id(SurgeryIdStr, SurgeryName))
+                )
+            )
+        )
+    ).
 
 % Converte os slots de tempo para o formato desejado
 convert_time_slots(TimeSlots, TimeSlotsFormatted) :-
@@ -55,14 +89,24 @@ format_time_slots(Stream, [(Start, End) | Rest]) :-
     ).
 
 doctorSchedules() :-
-    http_open('https://localhost:5001/api/AvailabilitySlots/GetAll', Reply,
+    http_open('https://10.9.10.55:5001/api/AvailabilitySlots/GetAll', Reply,
                [cert_verify_hook(cert_accept_any)]),
     json_read_dict(Reply, Data),
     writeln(Data),
     saveDoctorSchedules(Data).  % Salva os dados
 
-
+allSurgeries() :-
+    http_open('https://10.9.10.55:5001/api/OperationType/GetAll', Reply,
+               [cert_verify_hook(cert_accept_any)]),
+    json_read_dict(Reply, Data),
+    writeln(Data),
+    saveSurgeries(Data).  % Salva os dados
 
 % Remove todos os fatos de agenda_staff/3
 clear_agenda_staff :-
     retractall(agenda_staff(_, _, _)).
+
+% Remove todos os fatos de surgery/4
+clear_surgery :-
+    retractall(surgery(_, _, _, _)),
+    retractall(surgery_id(_, _)).
