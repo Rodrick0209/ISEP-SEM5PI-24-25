3b554937ffb60853b9ead7463deeeb83e2430d69
rodri <1220636@isep.ipp.pt>
2024-12-11 10:28:42 +0000

    improving genetic algorithm provided

--- Planning/geneticAlgorithm.pl
+++ Planning/geneticAlgorithm.pl
@@ -2,6 +2,8 @@
 :-dynamic population/1.
 :-dynamic prob_crossover/1.
 :-dynamic prob_mutation/1.
+:-dynamic percentage_individuals/1.
+:-dynamic lowerCostWanted/1.
 
 % task(Id,ProcessTime,DueTime,PenaltyWeight).
 task(t1,2,5,1).
@@ -13,6 +15,7 @@ task(t5,3,8,2).
 % tasks(NTasks).
 tasks(5).
 
+
 % parameters initialization
 initialize:-write('Number of new generations: '),read(NG), 			
     (retract(generations(_));true), asserta(generations(NG)),
@@ -23,17 +26,40 @@ initialize:-write('Number of new generations: '),read(NG),
 	(retract(prob_crossover(_));true), 	asserta(prob_crossover(PC)),
 	write('Probability of mutation (%):'), read(P2),
 	PM is P2/100, 
-	(retract(prob_mutation(_));true), asserta(prob_mutation(PM)).
+	(retract(prob_mutation(_));true), asserta(prob_mutation(PM)),
+    write('Percentage of best individuals to be kept for the next generation (%): '), read(P3),
+    PI is P3/100,
+    (retract(percentage_individuals(_));true), asserta(percentage_individuals(PI)),
+    write('Lower cost wanted: '), read(LowerCost),
+    (retract(lowerCostWanted(_));true), asserta(lowerCostWanted(LowerCost)),
+    write('Time limit (in seconds): '), read(TempoLimite),
+    (retract(time_limit(_));true), asserta(time_limit(TempoLimite)).
 
 generate:-
     initialize,
+    get_time(StartTime),
     generate_population(Pop),
     write('Pop='),write(Pop),nl,
     evaluate_population(Pop,PopValue),
     write('PopValue='),write(PopValue),nl,
     order_population(PopValue,PopOrd),
-    generations(NG),
-    generate_generation(0,NG,PopOrd).
+    lowerCostWanted(LowerCost),
+    ( PopOrd = [_*V|_]) ->
+    ( V =< LowerCost ->
+        write('Lower cost found!'), nl, !
+        ;
+        get_time(CurrentTime),
+        time_limit(TimeLimit),
+        ElapsedTime is CurrentTime - StartTime,
+        ( ElapsedTime > TimeLimit ->
+            write('Tempo limite atingido após '), write(ElapsedTime), write(' segundos.'), nl, !
+        ;
+            generations(NG),
+            generate_generation(0, NG, PopOrd, StartTime)
+        )
+    ).
+
+
 
 generate_population(Pop):-
     population(PopSize),
@@ -57,7 +83,7 @@ generate_individual([G],1,[G]):-!.
 generate_individual(TasksList,NumT,[G|Rest]):-
     NumTemp is NumT + 1, % to use with random
     random(1,NumTemp,N),
-    remove(N,TasksList,G,NewList),
+    remove(N,TasksList,G,NewList), %Remove o elemento na posicao N da lista TasksList e coloca em G
     NumT1 is NumT-1,
     generate_individual(NewList,NumT1,Rest).
 
@@ -98,16 +124,44 @@ bchange([X*VX,Y*VY|L1],[Y*VY|L2]):-
 
 bchange([X|L1],[X|L2]):-bchange(L1,L2).
     
-generate_generation(G,G,Pop):-!,
+generate_generation(G,G,Pop,_):-!,
 	write('Generation '), write(G), write(':'), nl, write(Pop), nl.
-generate_generation(N,G,Pop):-
+generate_generation(N,G,Pop,StartTime):-
 	write('Generation '), write(N), write(':'), nl, write(Pop), nl,
-	crossover(Pop,NPop1),
-	mutation(NPop1,NPop),
-	evaluate_population(NPop,NPopValue),
-	order_population(NPopValue,NPopOrd),
-	N1 is N+1,
-	generate_generation(N1,G,NPopOrd).
+	get_time(CurrentTime),
+    time_limit(TimeLimit),
+    ElapsedTime is CurrentTime - StartTime,
+    
+    ( ElapsedTime > TimeLimit ->
+        write('Tempo limite atingido após '), write(ElapsedTime), write(' segundos.'), nl,!
+    ;
+
+    random_permutation(Pop, ShuffledPop), % Shuffle the population to avoid the crossover being always between elements in the same position
+    crossover(ShuffledPop,NPop1),
+	
+    mutation(NPop1,NPop),
+	
+    evaluate_population(NPop,NPopValue),
+    
+    append(Pop, NPopValue, PopMergedWithDup), % Merge the old population with the new one
+    list_to_set(PopMergedWithDup, PopMerged), % Remove duplicates
+    order_population(PopMerged,PopMergedOrd), % Order the merged population
+    split_best_individuals(PopMergedOrd, BestIndividuals, RemainingIndividuals), % Split the best individuals from the remaining ones
+    length(BestIndividuals, NumBest),
+    population(PopSize),
+    
+    NumIndividuosAirBuscarRestoLista is PopSize - NumBest,
+    nl,
+    write('BestIndividuals='),write(BestIndividuals),
+	nl,
+    create_and_sort_remaining_individuals(RemainingIndividuals, NumIndividuosAirBuscarRestoLista, NextGenIndividuals), 
+    write('Rest of individuals for the population='),write(NextGenIndividuals),nl,nl,
+	append(BestIndividuals, NextGenIndividuals, NewPopulationNotOrded),
+    order_population(NewPopulationNotOrded, NewPopulation),
+    write('NewPopulation='),write(NewPopulation),nl,nl,
+    N1 is N+1,
+	generate_generation(N1,G,NewPopulation,StartTime)
+    ).
 
 generate_crossover_points(P1,P2):- generate_crossover_points1(P1,P2).
 
@@ -125,7 +179,8 @@ generate_crossover_points1(P1,P2):-
 crossover([ ],[ ]).
 crossover([Ind*_],[Ind]).
 crossover([Ind1*_,Ind2*_|Rest],[NInd1,NInd2|Rest1]):-
-	generate_crossover_points(P1,P2),
+	
+    generate_crossover_points(P1,P2),
 	prob_crossover(Pcruz),random(0.0,1.0,Pc),
 	((Pc =< Pcruz,!,
         cross(Ind1,Ind2,P1,P2,NInd1),
@@ -226,3 +281,59 @@ mutacao23(G1,1,[G2|Ind],G2,[G1|Ind]):-!.
 mutacao23(G1,P,[G|Ind],G2,[G|NInd]):-
 	P1 is P-1,
 	mutacao23(G1,P1,Ind,G2,NInd).
+
+
+
+melhores_n(Lista, N, Melhores) :-
+    order_population(Lista, Ordenada),
+    first_n(Ordenada, N, Melhores).
+
+
+% Pega os N primeiros elementos de uma lista
+first_n(_, 0, []) :- !. % Quando N é 0, a lista resultante é vazia
+first_n([X|Xs], N, [X|Ys]) :-
+    N > 0,
+    N1 is N - 1,
+    first_n(Xs, N1, Ys).
+
+
+split_best_individuals(Population, BestIndividuals, RemainingIndividuals) :-
+    percentage_individuals(Percentage),
+    length(Population, TotalSize),
+    N is max(1, round(TotalSize * Percentage)), % Calculate N accordingly with the percentage (min 1)
+    length(BestIndividuals, N),               
+    append(BestIndividuals, RemainingIndividuals, Population). % Divide a população
+
+
+
+
+% Gera o produto (IndList, Eval, Product) para cada indivíduo
+generate_random_products([], []).  % Caso base: lista vazia.
+
+generate_random_products([IndList * Eval | Rest], [(IndList * Product) | NewRest]) :-
+    random(Rand), 
+    Product is Eval * Rand,
+    generate_random_products(Rest, NewRest).
+
+% Chama o predicado para gerar os produtos aleatórios
+create_and_sort_remaining_individuals(RemainingIndividuals, N, NextGenIndividuals) :-
+    nl, nl,
+    generate_random_products(RemainingIndividuals, RandomizedProducts),
+    order_population(RandomizedProducts, SortedProducts),
+    write('Resto da lista SortedProducts='), write(SortedProducts), nl,nl,
+    write('Lista original='), write(RemainingIndividuals), nl,
+    extract_top_individuals(SortedProducts, N, RemainingIndividuals, NextGenIndividuals).
+
+extract_top_individuals([], _, _, []).
+extract_top_individuals(_, 0, _, []).
+extract_top_individuals([Ind * _ | RestSorted], N, RemainingIndividuals, [Ind * Eval | NewRest]) :-
+    N1 is N - 1,
+    member(Ind * Eval, RemainingIndividuals),
+    extract_top_individuals(RestSorted, N1, RemainingIndividuals, NewRest).
+
+
+parar_execucao :-
+    writeln('Time limit exceeded! Finishing the program...'),
+    abort.
+
+
