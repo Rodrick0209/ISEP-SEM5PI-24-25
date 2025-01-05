7df423e6f70f431fcd1d768d2f25b1661f9dfc7e
Miguel Oliveira <1211281@isep.ipp.pt>
2025-01-05 14:24:58 +0000

    fix: add new origin to CORS policy for Angular application

--- MastersData/src/Program.cs
+++ MastersData/src/Program.cs
@@ -169,7 +169,7 @@ namespace DDDSample1.Startup
                     options.AddPolicy("AllowAngularApp", builder =>
                     {
                         builder.WithOrigins("http://localhost:4200",
-                         "https://127.0.0.1:5501","http://127.0.0.1:5500","http://10.9.21.166","http://10.9.22.72","http://10.9.22.225") // Angular's URL
+                         "https://127.0.0.1:5501","http://127.0.0.1:5500","http://10.9.21.166","http://10.9.22.72","http://10.9.22.225","http://vs422.dei.isep.ipp.pt") // Angular's URL
                         .AllowAnyHeader()
                         .AllowAnyMethod()
                         .AllowCredentials();
